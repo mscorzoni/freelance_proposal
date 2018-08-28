18c5da1d036164fa577a9c4b14a469b92dbe1a96
Marcos Scorzoni <mlessa@gmail.com>
2018-08-28 21:46:41 +0100

    add mailer to proposals

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,13 @@
     client_email: 'test@test.com'
     )
 end
+
+Proposal.create!(
+    customer: "Google",
+    portfolio_url: 'http://mscorzoni-portfolio.herokuapp.com',
+    tools: 'Ruby on Rails, Angular 6 and Postgresql',
+    estimated_hours: 120,
+    hourly_rate: 120,
+    weeks_to_complete: 12,
+    client_email: 'test@test.com'
+    )
